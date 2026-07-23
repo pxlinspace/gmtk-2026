965a22f8321a67fa7e938bcf74787624aa67f6b1
deltea <thcheetah777@gmail.com>
2026-07-23 10:58:05 +0800

    spawn tiles

---
+++ scenes/levels/level.gd
@@ -0,0 +1,20 @@
+class_name Level extends Node3D
+
+const tile_scene = preload("res://scenes/tile/tile.tscn")
+
+@onready var level_map: GridMap = $LevelMap
+
+func _ready() -> void:
+	spawn_tiles()
+
+func spawn_tiles() -> void:
+	var cells: Array[Vector3i] = level_map.get_used_cells()
+	var tile_container := Node.new()
+	add_child(tile_container)
+	for cell_pos in cells:
+		var idx := level_map.get_cell_item(cell_pos)
+		var tile := tile_scene.instantiate() as Tile
+		tile.countdown = int(level_map.mesh_library.get_item_name(idx))
+		tile_container.add_child(tile)
+		tile.global_position = level_map.to_global(cell_pos)
+		level_map.set_cell_item(cell_pos, GridMap.INVALID_CELL_ITEM)
---
+++ scenes/tile/tile.gd
@@ -0,0 +1,3 @@
+class_name Tile extends Area3D
+
+var countdown: int = 0
