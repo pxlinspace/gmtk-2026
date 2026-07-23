f402f9883690c2a2b38e9b827dec77b64566a391
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-07-22 23:47:52 -0700

    new anchor component + flag can now be grabbed

---
+++ scenes/components/anchor.gd
@@ -0,0 +1,5 @@
+class_name Anchor extends Node3D
+
+
+func _ready() -> void:
+	top_level = true
--- scenes/objects/flag/flag.gd
+++ scenes/objects/flag/flag.gd
@@ -1,18 +1,23 @@
 class_name Flag extends Area3D
 
 var position_tween: Tween
+var is_grabbed := false
+
 @onready var grid_position: Vector3i = Vector3i(position)
 @onready var anchor: Node3D = $Anchor
+@onready var tile_sprite: TileSprite = $Anchor/TileSprite
 
 
 func grab() -> void:
 	monitoring = false
-	animate_anchor_raise()
+	is_grabbed = true
+	animate_raise()
 
 
-func animate_anchor_raise() -> void:
+func animate_raise() -> void:
 	var tween := create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 	tween.tween_property(anchor, "position", anchor.position + Vector3.UP, 0.2)
+	tile_sprite.animation_player.play("bounce")
 
 
 func change_flag_pos(new_pos: Vector3i) -> void:
@@ -24,5 +29,6 @@ func change_flag_pos(new_pos: Vector3i) -> void:
 func animate_to_grid_position() -> void:
 	if position_tween: position_tween.kill()
 	position_tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-	var target_pos := Vector3(grid_position) + Vector3(0, 0, 0)
+	var offset := Vector3.UP if is_grabbed else Vector3.ZERO
+	var target_pos := Vector3(grid_position) + offset
 	position_tween.tween_property(anchor, "global_position", target_pos, 0.2)
