09e8a776da8749ef3acd743151d74f763ce257d9
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-07-23 16:36:01 -0700

    added slug sprite

--- scenes/enemies/pattern_enemy.gd
+++ scenes/enemies/pattern_enemy.gd
@@ -3,6 +3,7 @@ class_name PatternEnemy extends BaseEnemy
 @export var pattern: Array[Vector3i] = []
 
 @onready var arrow: Sprite3D = $Arrow
+@onready var next_square: Sprite3D = $NextSquare
 
 var position_tween: Tween
 
@@ -19,6 +20,11 @@ func move_to_pos(new_pos: Vector3i) -> void:
 
 func move(curr_timestep: int) -> void:
 	var curr_move := pattern[curr_timestep % pattern.size()]
+	if curr_move.x > 0:
+		tile_sprite.flip_h = false
+	elif curr_move.x < 0:
+		tile_sprite.flip_h = true
+	
 	move_to_pos(grid_position + curr_move)
 	update_arrow(curr_timestep)
 
@@ -26,9 +32,12 @@ func move(curr_timestep: int) -> void:
 func update_arrow(curr_timestep: int) -> void:
 	var curr_move := pattern[curr_timestep % pattern.size()]
 	var next_move := pattern[(curr_timestep + 1) % pattern.size()]
+	
 	arrow.position = Vector3(grid_position) + Vector3(0.5, 0, 0.5)
 	arrow.position += Vector3(next_move * 0.5)
 	arrow.rotation.y = atan2(curr_move.x, curr_move.z)
+	next_square.position = Vector3(grid_position + next_move) + Vector3(0.5, 0, 0.5)
+	
 
 
 func animate_to_grid_position() -> void:
