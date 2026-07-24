e65d1c8271ee43d4a1f8c2c7f59ed2ef58507252
deltea <thcheetah777@gmail.com>
2026-07-25 00:56:54 +0800

    create level select and main menu scenes

---
+++ scenes/screens/main_menu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu extends Node3D
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	pass
+
+
+func _on_level_select_button_pressed() -> void:
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	pass
