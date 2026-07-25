3f07e82e0083ac70de57cb38bb81ea91d7666245
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-07-25 07:26:40 -0700

    working on title

---
+++ scenes/screens/main_menu/title_sprites.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const RADIAN_OFFSET: float = 0.75
+
+@export var radians: float = 0.0
+@onready var title_sprites: Node2D = $TitleSprites
+
+
+func _process(delta: float) -> void:
+	radians += delta * 2.0
+	if radians > TAU:
+		radians = radians - TAU
+	
+	for i in get_children().size():
+		var sprite: Sprite2D = get_child(i)
+		sprite.position.y = sin(radians + i * RADIAN_OFFSET) * 7.0
