58e58e12d64a5c4f59db0291c1c0ae274e301fba
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-07-25 08:53:18 -0700

    wip planet + tiles now shrink when dropped

---
+++ scenes/objects/planet/planet.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var clouds: MeshInstance3D = $Clouds
+@onready var clouds_2: MeshInstance3D = $Clouds2
+
+
+func _process(delta: float) -> void:
+	clouds.rotation.y += delta * 0.02
+	clouds_2.rotation.y += delta * 0.04
+	
+	
